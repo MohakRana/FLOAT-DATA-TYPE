535ab3c10aa495e0121ccc2110b24f23a149ca5d
MohakRana <mohakrana79@gmail.com>
2025-07-01 03:37:49 +0530

    Add files via upload

---
+++ floatdatatype.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+float x= 3.14;
+printf("%f",x);
+return 0;
+
+
+
+
+
+
+}
---
+++ floatdatatype1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+float x = 47;
+float y = 3;
+float z = x/y;
+printf("%f",z);
+return 0;
+
+
+
+
+
+
+
+}
